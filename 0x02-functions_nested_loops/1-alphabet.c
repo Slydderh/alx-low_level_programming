110f6619c7e39c421626d71261fb7a6413b5041c
slydderh <zephayuba9126@gmail.com>
2022-10-24 04:16:12 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
+
