c79cbfe627dccdb3f48209048a2043951bef8d4e
slydderh <zephayuba9126@gmail.com>
2022-10-24 03:48:39 -0700

    task

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int n);
+extern int print_last_digit(int n);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int a, int b);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
+
+
+#endif
+
